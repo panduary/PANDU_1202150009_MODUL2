73c1e22d1450f60a785200acb6785b795c22af32
panduary <pandu_ary@yahoo.com>
2018-02-18 14:00:32 +0700

    PANDU_1202150009_MODUL2

---
+++ app/src/main/java/com/example/android/pandu_1202150009_modul2/Splash.java
@@ -0,0 +1,25 @@
+package com.example.android.pandu_1202150009_modul2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Toast.makeText(this,"PANDU ARY WIJAYA_1202150009",Toast.LENGTH_SHORT).show();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000L); //3000 L = 3 detik
+    }
+}
